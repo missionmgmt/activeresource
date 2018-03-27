35993f7b794c0e2162d1ad94024b04db54c8234b
Ben Mannell <benjamin.mannell@yahoo.ca>
2018-03-27 15:21:01 -0400

    Revert to old method

--- lib/active_resource/version.rb
+++ lib/active_resource/version.rb
@@ -2,8 +2,8 @@ module ActiveResource
   module VERSION #:nodoc:
     MAJOR = 4
     MINOR = 1
-    TINY  = 4
-    PRE   = nil
+    TINY  = 0
+    PRE   = 1
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
