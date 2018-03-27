c43d2e8dd0a1170857daf0ba64ad5b2791a3dc5a
Ben Mannell <benjamin.mannell@yahoo.ca>
2018-03-27 15:24:43 -0400

    Full update

--- lib/active_resource/version.rb
+++ lib/active_resource/version.rb
@@ -2,8 +2,8 @@ module ActiveResource
   module VERSION #:nodoc:
     MAJOR = 4
     MINOR = 1
-    TINY  = 0
-    PRE   = 2
+    TINY  = 1
+    PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
